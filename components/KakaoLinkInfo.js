e5d4efa70b5a47c831b04505997b7fd28e6738ed
cjh980402 <9804cjh@naver.com>
2020-09-26 21:35:04 +0900

    app ID value change

--- components/KakaoLinkInfo.js
+++ components/KakaoLinkInfo.js
@@ -4,7 +4,7 @@ const Component_1 = require("../Component");
 class KakaoLinkInfo extends Component_1.default {
     constructor() {
         super();
-        this.appID = '432166'; // 카카오톡 디벨로퍼에서의 appID 값
+        this.appID = 'null'; // 카카오톡 디벨로퍼에서의 appID
         this.templateID = '3139';
     }
     toJson() {
